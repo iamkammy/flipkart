a1a5267813cb44bdb5577c7809f6beff6135abeb
Kamran Khan <kamran.jmi12@gmail.com>
2024-12-30 02:25:26 +0530

    removed dummy route /profile

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,8 +5,4 @@ import authController from "../controller/auth";
 router.post("/signin", authController.signin);
 router.post("/signup", authController.signup);
 
-router.post("/profile", authController.requireSignin, async (req: Request, res: Response) => {
-  res.status(200).json({ profile: "Profile" });
-});
-
 export default router;
