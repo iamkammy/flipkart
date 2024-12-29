34f5d8ab849487e7bb20a9e64a402dbe1e940890
Kamran Khan <kamran.jmi12@gmail.com>
2024-12-30 03:32:52 +0530

    Add admin authentication routes to the main application

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { logger } from "./utils/logger";
 
 // Routes
 import authRoutes from "./routes/auth";
+import adminAuthRoutes from "./routes/admin/auth";
 
 dotenv.config(); // Load environment variables from .env file
 
@@ -14,9 +15,12 @@ const port = process.env.PORT || 3000;
 // Connect to DataBase
 connectDB();
 
-// middleware
+// Third-party middleware
 app.use(express.json());
+
+// custom middlewares
 app.use("/api", authRoutes);
+app.use("/api", adminAuthRoutes);
 
 app.listen(port, () => {
   logger.success(`[server]: Server is running at http://localhost:${port}`);
