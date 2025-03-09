91a061ca9d8c8f844cfb2bd15e4088f8658959c3
basit_ali <alibasit78@gmail.com>
2025-03-10 01:36:57 +0530

    updated cartException

--- src/exceptions/cartException.js
+++ src/exceptions/cartException.js
@@ -21,3 +21,10 @@ try {
 } catch (error) {
     handleCartException(error);
 }
+
+// Output:
+// CartException: Item not found in the cart. 
+    // In this example, we define a custom exception class called  CartException  that extends the built-in  Error  class. The  CartException  class has a custom  name  property and a constructor that sets the error message. We also define a function called  handleCartException  to handle instances of  CartException . 
+    // When an error is thrown using  throw new CartException("Item not found in the cart.") , we catch the error and call the  handleCartException  function to handle the exception. If the error is not an instance of  CartException , we re-throw the error. 
+    // 5. Using Custom Error Classes in Node.js 
+    // In Node.js, you can define custom error classes and use them in your applications. Here's an example of how you can define and use a custom error class in a Node.js application:
